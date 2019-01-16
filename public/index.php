7008b2d6e36e24671a3c8e069f221081c8bb9ff2
Alexandre Briffaut <briffaut.alexandre62@gmail.com>
2019-01-16 09:15:53 +0100

    Adding ,  and

--- public/index.php
+++ public/index.php
@@ -1,9 +1,27 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
-$h = new Hero('Moïse', 100, 100, 3);
+$h = new Hero('Alexandre', 100, 100, 3);
 echo $h->getName().'<br>';
 echo $h->getHP().'<br>';
 echo $h->getMP().'<br>';
 echo ($h->isDead() ? 'oui': 'non').'<br>';
 echo $h->getLives().'<br>';
+
+$v = new Vilain('Barbare', 10, 10);
+echo $v->getName(). '<br>';
+echo $v->getHP(). '<br>';
+echo $v->getMP(). '<br>';
+echo ($v->isDead() ? 'oui': 'non'). '<br>';
+
+$f = new Fight();
+$f->setFighters($h, $v);
+
+while ( !$f->isOver()){
+    $f->round();
+}
+
+$winner = $f->getWinner();
+echo "The winner is {$winner->getName()}<br>";
+echo $winner->getHP().'<br>';
+echo $winner->getMP().'<br>';
